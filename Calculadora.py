bbec0eb4c2b1628b68c62df9548627b28f01875c
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2022-09-26 20:14:25 -0300

    Create Calculadora.py

---
+++ Calculadora.py
@@ -0,0 +1,38 @@
+def calculadora():
+    operacao = input('''
+Por favor escolha com qual operador você deseja trabalhar
+1 para adição
+2 para subtração
+3 para multiplicação
+4 para divisão
+''')
+
+    if operacao == '1':
+        n1 = int(input("Insira o primeiro número: "))
+        n2 = int(input("Insira o segundo número: "))
+        print('{} + {} = '.format(n1, n2))
+        print(n1 + n2)
+
+    elif operacao == '2':
+        n1 = int(input("Insira o primeiro número: "))
+        n2 = int(input("Insira o segundo número: "))
+        print('{} - {} = '.format(n1, n2))
+        print(n1 - n2)
+
+    elif operacao == '3':
+        n1 = int(input("Insira o primeiro número: "))
+        n2 = int(input("Insira o segundo número: "))
+        print('{} * {} = '.format(n1, n2))
+        print(n1 * n2)
+
+    elif operacao == '4':
+        n1 = int(input("Insira o primeiro número: "))
+        n2 = int(input("Insira o segundo número: "))
+        print('{} / {} = '.format(n1, n2))
+        print(n1 / n2)
+
+    else:
+        print("Você não escolheu um operador válido")
+        exit()
+
+calculadora()
