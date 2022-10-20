e307d0f07011dafa753abccdc8c8664ae6e6238c
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2022-10-20 15:10:12 -0300

    Create Manipulação strings.py

---
+++ Manipulação strings.py
@@ -0,0 +1,9 @@
+texto1 =  "Olá pessoas."
+print (texto1)
+
+texto1  =  texto1 . replace ("pessoas" , "mundo") # Substitui 'pessoas' por 'mundo' no texto
+print (texto1)
+
+print (texto1.find('m')) # Localizar posição do caractere
+
+print ("Continuem"  +  " o aprendizado!") #Concatenação
