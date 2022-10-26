2470436921bf251543c1e89b9c48ce080c5988e2
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2022-10-26 19:49:08 -0300

    Update exercicio python.py

--- exercicio python.py
+++ exercicio python.py
@@ -1,12 +1,14 @@
-def Notas ():
+def Notas():
     n1 = int(input("Insira o primeira nota: "))
     n2 = int(input("Insira o segunda nota: "))
     media = (n1+n2)/2
     print('Sua média é ', media)
     if media <= 7:
-        x = 21
-        
         print('Aluno Reprovado')
+        rest = 14 - media
+        print('O aluno precisa tirar', rest, 'na recuperação para passar!')
     else:
         print('Aluno aprovado')
+
+
 Notas()
