39b2569207ecd91bc63dce2da6059cf21ca0347f
GustavoLorena <113564100+GustavoLorena@users.noreply.github.com>
2023-02-13 19:47:39 -0300

    Create HTTPGET.js

---
+++ HTTPGET.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/get', (request, response) => {
+  response.send('Recebi uma requisição GET');
+});
+
+app.post('/post', (request, response) => {
+  response.send('Recebi uma requisição POST');
+});
+
+app.listen(8080, () => {
+  console.log('Servidor iniciado na porta 8080');
+});
+
+// localhost/8080
+// localhost/8080/get
+// localhost/8080/post
